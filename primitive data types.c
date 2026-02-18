026c84674c18ee266d1bfab742feaace3df14070
112534060afra <112534060@kanchiuniv.ac.in>
2026-02-18 15:33:21 +0500

    Add files via upload

---
+++ primitive data types.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+printf("int=%d bytes\n",sizeof(int));
+printf("char=%d bytes\n",sizeof(char));
+printf("float=%d bytes\n",sizeof(float));
+printf("double=%d bytes\n",sizeof(double));
+return 0;
+}
