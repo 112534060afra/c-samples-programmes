f11a7ba40f7bfd911144efb9213678af2731cd3e
112534060afra <112534060@kanchiuniv.ac.in>
2026-02-18 15:28:39 +0500

    Add files via upload

---
+++ 112534060.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the first value:");
+scanf("%d",&a);
+printf("enter the second value:");
+scanf("%d",&b);
+if(a==b)
+printf("both statement are equal:");
+else
+printf("both statement are not equal:");
+return 0;                                                                                           
+}
+
